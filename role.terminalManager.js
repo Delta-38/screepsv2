28d6aa4b12ed6d54b5f1c31c767cc0caf446ca1c
Paolo Manili <paolomanili@gmail.com>
2016-12-15 08:25:09 +0100

    Fixed an error in main miner spawn code. I Really need to replace these big switch blocks soon. Added a few more utility functions to Spawn prototype. Added a new stub for a role in scout role (For Portal exploration) Did my first few transactions thanks to the terminal loading script Forgot to add said file!

---
+++ role.terminalManager.js
@@ -0,0 +1,69 @@
+/**
+ * Created by paolo on 14/12/2016.
+ */
+
+var terminalManager = {
+
+    run:function(creep){
+        var gathering = creep.memory.gathering;
+        var delivering = creep.memory.delivering;
+
+        //IF DELIVERING GO TO TERMINAL
+
+        //IF GATHERING FIND A RESOURCE TO CARRY
+            //FETCH THE RESOURCE TO CARRY
+                //IF FULL DELIVER
+        creep.say("TMAN");
+        if(creep.load()!=0){
+            creep.say('LOAD'+creep.load());
+            if(creep.pos.isNearTo(creep.room.terminal)){
+                for(var carry in creep.carry){
+                    var res = creep.transfer(creep.room.terminal,carry);
+                    creep.say('UL'+res);
+                   // break;
+                }
+            }else{
+                creep.moveTo(creep.room.terminal);
+            }
+        }else{
+            //GATHERING
+            creep.say('GTHER');
+            var resource = null;
+            if((resource = this.getNeededResource(creep))!=null){
+                creep.say('GETRES'+resource);
+                if(!creep.pos.isNearTo(creep.room.storage.pos)){
+                    creep.moveTo(creep.room.storage);
+                    creep.say('GTOSTO');
+
+                }else{
+                    creep.withdraw(creep.room.storage,resource);
+                }
+            }
+        }
+
+
+    },
+
+
+
+    getNeededResource:function(creep){
+        if(creep.room.storage && creep.room.terminal){
+            creep.say('Ook!');
+            for(var resourceKey in creep.room.storage.store){
+                creep.say(resourceKey);
+                creep.log("Storage Resource: "+resourceKey);
+                var termRes = creep.room.terminal.store[resourceKey];
+                creep.say('a'+termRes);
+                if(!termRes || (termRes && termRes < 80000 && termRes < creep.room.storage.store[resourceKey])){
+                    creep.memory.fetching = resourceKey;
+                    return resourceKey;
+                }
+
+            }
+        }
+        return null;
+    }
+
+};
+
+module.exports = terminalManager;
